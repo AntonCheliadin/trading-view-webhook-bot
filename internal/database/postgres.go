de7e4db8cf5592ffc8b40b7b5603fdc333d0277d
antoncheliadin <ac@medicosearch.ch>
2025-04-28 23:25:13 +0200

    refactor connecting to db

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -11,18 +11,28 @@ import (
 )
 
 func NewPostgresConnection() (*sqlx.DB, error) {
-	connStr := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-	)
+
+	connStr := os.Getenv("DATABASE_URL")
+
+	if connStr == "" {
+		connStr = fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+		)
+	}
 
 	db, err := sqlx.Connect("postgres", connStr)
 	if err != nil {
 		return nil, err
+	}
+
+	// Verify connection
+	if err = db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %v", err)
 	}
 
 	initMigrations(db)
