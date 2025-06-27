7d5761480417d52ec3873d8d0f5225a4b0ad3284
antoncheliadin <ac@medicosearch.ch>
2025-06-27 15:21:30 +0200

    add strategies migration

---
+++ migrations/005_create_trading_strategies.sql
@@ -0,0 +1,19 @@
+-- +migrate Up
+INSERT INTO trading_strategies (name, description, tag, created_at, updated_at)
+VALUES ('ATOM 4h', 'SSL WAE_LB ATR LONG+SHORT', 'ATOM_SSL_WAE_LONG_SHORT', NOW(), NOW());
+
+-- +migrate Up
+INSERT INTO trading_strategies (name, description, tag, created_at, updated_at)
+VALUES ('AVAX 4h', 'SSL WAE_LB ATR LONG+SHORT', 'AVAX_SSL_WAE_LONG_SHORT', NOW(), NOW());
+
+-- +migrate Up
+INSERT INTO trading_strategies (name, description, tag, created_at, updated_at)
+VALUES ('BTC 4h', 'Test Trading strategy (BTC simple pullback 4H)', 'BTC_SIMPLE_PULLBACK', NOW(), NOW());
+
+-- +migrate Up
+INSERT INTO trading_strategies (name, description, tag, created_at, updated_at)
+VALUES ('BTC 1D', 'ChatGpt without SL and TP (BTC ChatGpt 1D)', 'BTC_CHAT_GPT', NOW(), NOW());
+
+-- +migrate Up
+INSERT INTO trading_strategies (name, description, tag, created_at, updated_at)
+VALUES ('ETH 4H', 'SSL WAE_LB ATR LONG+SHORT (ETH 4H):', 'ETH_SSL_WAE_LONG_SHORT', NOW(), NOW());
