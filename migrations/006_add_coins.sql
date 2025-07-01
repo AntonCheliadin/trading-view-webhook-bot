6fc899de9a4e236e27e5577213a556c39abb2d92
antoncheliadin <ac@medicosearch.ch>
2025-07-01 15:41:16 +0200

    add coins migration

---
+++ migrations/006_add_coins.sql
@@ -0,0 +1,4 @@
+-- +migrate Up
+INSERT INTO coins (coin_name, symbol) VALUES ('Atom', 'ATOMYSDT');
+INSERT INTO coins (coin_name, symbol) VALUES ('Avax', 'AVAXUSDT');
+INSERT INTO coins (coin_name, symbol) VALUES ('Ethereum', 'ETHUSDT');
